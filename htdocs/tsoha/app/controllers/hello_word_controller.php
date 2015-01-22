3f8b79429ee215b795b8545d91a994269b7e9213
Olli P Eskola <opeskola@svm-19.cs.helsinki.fi>
2015-01-22 18:58:27 +0200

    lisatty sh.tiedostot oikeaan paikkaan

--- htdocs/tsoha/app/controllers/hello_word_controller.php
+++ htdocs/tsoha/app/controllers/hello_word_controller.php
@@ -3,11 +3,31 @@
   class HelloWorldController extends BaseController{
 
     public static function index(){
-   	  self::render_view('home.html');
+   	  echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä	
-      echo 'Hello World!';
+      self::render_view('helloworld.html');
+    }
+    
+    public static function login(){
+      self::render_view('suunnitelmat/login.html');
+    }
+    
+    public static function new_questions_list(){
+      self::render_view('suunnitelmat/new_questions_list.html');
+    }
+    
+    public static function themas(){
+      self::render_view('suunnitelmat/themas.html');
+    }
+    
+    public static function make_question(){
+      self::render_view('suunnitelmat/make_question.html');
+    }
+    
+    public static function first_page(){
+      self::render_view('suunnitelmat/first_page.html');
     }
   }
--- htdocs/tsoha/config/routes.php
+++ htdocs/tsoha/config/routes.php
@@ -7,3 +7,23 @@
   $app->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+  
+  $app->get('/suunnitelmat/login', function() {
+    HelloWorldController::login();
+  });
+  
+  $app->get('/suunnitelmat/new_questions_list', function() {
+    HelloWorldController::new_questions_list();
+  });
+  
+  $app->get('/suunnitelmat/themas', function() {
+    HelloWorldController::themas();
+  });
+  
+  $app->get('/suunnitelmat/make_question', function() {
+    HelloWorldController::make_question();
+  });
+  
+  $app->get('/suunnitelmat/first_page', function() {
+    HelloWorldController::first_page();
+  });  
