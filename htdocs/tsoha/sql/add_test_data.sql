6ca54d378e0256fac3f966d04d5ef321c3e188bc
Olli P Eskola <opeskola@svm-19.cs.helsinki.fi>
2015-01-24 13:51:18 +0200

    testidata lisatty

--- htdocs/tsoha/sql/add_test_data.sql
+++ htdocs/tsoha/sql/add_test_data.sql
@@ -1,12 +1,16 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
-INSERT INTO Opiskelija (nimi, osoite, syntymaAika, tiedekunta, aloitusvuosi, salasana)
-VALUES ('Matti Nieminen', 'Hyvinkaa', '1983-05-26', 'Matematiikka', '2007', 'sljsdljfieerrd');
+INSERT INTO Opiskelija (opiskelijaNro, nimi, osoite, syntymaAika, tiedekunta, aloitusvuosi, salasana)
+VALUES ('1002344', 'Matti Nykanen', 'Hyvinkaa', '1973-12-05', 'Matematiikka', '2007', 'saskijrcdsesa');
 
-INSERT INTO OpintoOhjaaja (nimi, osoite, syntymaAika, salasana)
-VALUES ('Keijo Miettinen', 'Helsinginkatu 5 A', '1973-08-14', 'sljdfdhkiuyy654rf');
+INSERT INTO OpintoOhjaaja (ID, nimi, osoite, syntymaAika, salasana)
+VALUES ('50001', 'Keijo Miettinen', 'Helsinginkatu 5', '1960-06-23', 'fr4FGE5guugb');
 
-INSERT INTO Aihepiiri (aihe)
-VALUES ('opintotuki');
+INSERT INTO Aihepiiri (ID, aihe) VALUES ('1', 'Opintotuki');
+INSERT INTO Aihepiiri (ID, aihe) VALUES ('2', 'Joo-opinnot');
 
-INSERT INTO Kysymys (aiheID, kysyja, vastaaja, kysymys)
-VALUES ('1', '1', '1', 'mitä kuuluu?');
+
+INSERT INTO Kysymys (ID, opiskelijaNro, sisalto, pvm, status)
+VALUES ('1', '1002344', 'Kuinka paljon opintopisteita tarvitaan, jotta saa opintotukea?', '2015-01-10', TRUE);
+
+INSERT INTO Vastaus (ID, vastaaja, kysymys, aihe, sisalto, pvm)
+VALUES ('1', '50001', '1', '1', 'Opistotukeen tarvitsee vähintään 20 opintopistettä lukakaudessa.', '2015-01-15');
